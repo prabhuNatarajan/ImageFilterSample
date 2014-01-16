9393989c2da6a5665da2120747ee634b79adf971
prabhunatarajan <prabhu.natarajan@linkedteams.com>
2014-01-16 19:18:37 +0530

    Class file added.

---
+++ ImageFilterSample/ImageFilterSample/UIimage+FilterCompositions.h
@@ -0,0 +1,26 @@
+//
+//  UIimage+FilterCompositions.h
+//  ImageFilterSample
+//
+//  Created by Apple on 16/01/14.
+//  Copyright (c) 2014 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (FilterCompositions)
+
+- (id)trackTime:(NSString *)method;
+- (id)e1;
+- (id)e2;
+- (id)e3;
+- (id)e4;
+- (id)e5;
+- (id)e6;
+- (id)e7;
+- (id)e8;
+- (id)e9;
+- (id)e10;
+- (id)e11;
+
+@end
---
+++ ImageFilterSample/ImageFilterSample/UIimage+FilterCompositions.m
@@ -0,0 +1,13 @@
+//
+//  UIimage+FilterCompositions.m
+//  ImageFilterSample
+//
+//  Created by Apple on 16/01/14.
+//  Copyright (c) 2014 Apple. All rights reserved.
+//
+
+#import "UIimage+FilterCompositions.h"
+
+@implementation UIimage_FilterCompositions
+
+@end
